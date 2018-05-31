db17b79277a850d166643b7d955a6699f33b5fba
s16397 <s16397@pjwstk.edu.pl>
2018-05-31 22:09:17 +0200

    Spark demo with sample WIG20 data

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ADD_Proj
+
+To run Main on local Spark instance execute:
+1. sbt package
+2. %SPARK_HOME_DIR%/spark-submit spark-submit target/scala-2.11/add_proj_2.11-0.1.jar 
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,18 @@
+import org.apache.spark.sql.SparkSession
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().appName("SimpleApplication").getOrCreate()
+
+    val wigDataFrame = spark.read
+      .format("csv")
+      .option("header", true)
+      .option("mode", "DROPMALFORMED")
+      .load("../ADD_Proj/src/main/resources/wig20_test.csv")
+
+    wigDataFrame.show()
+    spark.stop()
+  }
+
+}
